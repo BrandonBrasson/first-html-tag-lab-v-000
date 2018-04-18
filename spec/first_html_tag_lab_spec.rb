7da4d9607132b9d4a0fc17170249e08407ee8463
Brandon Brasson <rocksong622@gmail.com>
2018-04-18 23:34:51 +0000

    automatically backed up by learn

--- spec/first_html_tag_lab_spec.rb
+++ spec/first_html_tag_lab_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Your First HTML Tag' do
     expect(h1).to_not be_nil, hint(1, h1)
   end
 
-  it "the H1 contains 'Hello, World!'" do
+ puts it "the H1 contains 'Hello, World!'" do
     h1_content = parsed_html.search('h1').text
 
     expect(h1_content).to eq('Hello, World!'), hint(2, h1_content)
